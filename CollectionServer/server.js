03cadb42841df67f1ce08de5057967932228a6f9
Photon cat <lkstc112233@users.noreply.github.com>
2022-07-02 01:56:22 -0700

    Added a basic server file.

---
+++ CollectionServer/server.js
@@ -0,0 +1,18 @@
+const db = require('./database/mysql');
+
+db.init().then(() => {
+    console.log('table created');
+}).catch((err) => {
+    console.error(err);
+    process.exit(1);
+});
+
+const gracefulShutdown = () => {
+    db.teardown()
+        .catch(() => {})
+        .then(() => process.exit());
+};
+
+process.on('SIGINT', gracefulShutdown);
+process.on('SIGTERM', gracefulShutdown);
+process.on('SIGUSR2', gracefulShutdown); // Sent by nodemon
