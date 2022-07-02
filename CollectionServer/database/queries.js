80d18689d7b5dea6b98f5727c56369b4a2f9f082
Photon cat <lkstc112233@users.noreply.github.com>
2022-07-02 01:56:09 -0700

    Fixed an issue for table creation

--- CollectionServer/database/queries.js
+++ CollectionServer/database/queries.js
@@ -1,13 +1,15 @@
 const CREATE_TABLES = `CREATE TABLE IF NOT EXISTS card_infos (
-    scryfall_id VARCHAR(36) PRIMARY KEY,
-    card_name VARCHAR(255) KEY,
+    scryfall_id VARCHAR(36),
+    card_name VARCHAR(255),
     card_printed_name VARCHAR(255),
     lang VARCHAR(10),
     scryfall_api_uri VARCHAR(70),
     scryfall_card_url VARCHAR(255),
     scryfall_image_uri VARCHAR(255),
     version VARCHAR(50),
-    reference_usd_cent_price INT
+    reference_usd_cent_price INT,
+    PRIMARY KEY(scryfall_id),
+    UNIQUE INDEX(scryfall_id)
 ) DEFAULT CHARSET utf8mb4`;
 
 module.exports = {
