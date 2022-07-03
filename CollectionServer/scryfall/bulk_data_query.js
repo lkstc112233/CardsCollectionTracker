fe2c28f096aa8542fd01d5262317f575b24f2c13
Photon cat <lkstc112233@users.noreply.github.com>
2022-07-03 04:27:08 -0700

    Integrate sql with card loader.

--- CollectionServer/scryfall/bulk_data_query.js
+++ CollectionServer/scryfall/bulk_data_query.js
@@ -1,6 +1,7 @@
 const got = require('got');
 const { parser } = require('stream-json/Parser');
 const { streamArray } = require('stream-json/streamers/StreamArray')
+const db = require('../database/mysql');
 
 function getAllCardsUrl() {
     return got.get('https://api.scryfall.com/bulk-data', {responseType: 'json'})
@@ -28,7 +29,13 @@ async function handleAllCards() {
         var objectCounter = 0;
         pipeline.on('data', data => {
             if (data.value?.object === 'card') {
-                ++objectCounter
+                ++objectCounter;
+                updateCard(data.value).catch(err => {
+                    console.log('Error happened while handling all cards: ' + 
+                    err.message + 
+                    ', handling card ' + 
+                    data.value.name);
+                });
             }
         });
         pipeline.on('end', () => {
@@ -36,6 +43,23 @@ async function handleAllCards() {
             resolve(objectCounter);
         });
     });
+}
+
+async function updateCard(cardData) {
+    optionalArgs = {}
+    if ('printed_name' in cardData) {
+        optionalArgs.card_printed_name = cardData.printed_name;
+    }
+    if ('image_uris' in cardData && 'png' in cardData.image_uris && cardData.image_uris.png) {
+        optionalArgs.scryfall_image_uri = cardData.image_uris.png;
+    }
+    if ('finishes' in cardData && cardData.finishes.length > 1) {
+        optionalArgs.version_string = cardData.finishes.join('|');
+    }
+    if ('prices' in cardData && 'usd' in cardData.prices && cardData.prices.usd) {
+        optionalArgs.reference_usd_cent_price = cardData.prices.usd * 100;
+    }
+    return db.updateCardMetadata(cardData.id, cardData.name, cardData.lang, cardData.uri, cardData.scryfall_uri, optionalArgs);
 }
 
 module.exports = {handleAllCards};
