b5ad9f9e2dfb54fcd3e736ccfe36a63ec6300e06
Photon cat <lkstc112233@users.noreply.github.com>
2022-07-03 02:25:23 -0700

    Added basic handling for bulk data.

--- CollectionServer/scryfall/bulk_data_query.js
+++ CollectionServer/scryfall/bulk_data_query.js
@@ -1,7 +1,9 @@
-const got = require("got");
+const got = require('got');
+const { parser } = require('stream-json/Parser');
+const { streamArray } = require('stream-json/streamers/StreamArray')
 
 function getAllCardsUrl() {
-    got.get('https://api.scryfall.com/bulk-data', {responseType: 'json'})
+    return got.get('https://api.scryfall.com/bulk-data', {responseType: 'json'})
         .then(res => {
             const headerDate = res.headers && res.headers.date ? res.headers.date : 'no response date';
             console.log('Status Code:', res.statusCode);
@@ -10,11 +12,28 @@ function getAllCardsUrl() {
             const data = res.body.data.find(bulk_data => bulk_data.type === 'all_cards');
             console.log('Got data');
             if (data) {
-                console.log('All cards data url: ' + data.download_uri);
+                return data.download_uri;
             }
-            console.log('Finished processing dataList');
+            throw 'bulk_data not available at this time.';
         })
         .catch(err => {
             console.log('Scryfall api request error: ' + err.message);
         });
+}
+
+async function handleAllCards() {
+    var bulk_data_url = await getAllCardsUrl();
+    const pipeline = got.stream(bulk_data_url).pipe(parser()).pipe(streamArray());
+    return new Promise(function(resolve, reject) {
+        var objectCounter = 0;
+        pipeline.on('data', data => {
+            if (data.value?.object === 'card') {
+                ++objectCounter
+            }
+        });
+        pipeline.on('end', () => {
+            console.log(`Found ${objectCounter} objects.`);
+            resolve(objectCounter);
+        });
+    });
 }
