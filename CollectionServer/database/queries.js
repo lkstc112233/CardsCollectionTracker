1451dbc6c85c2be688f55be304cab3d5f816cddb
Photon cat <lkstc112233@users.noreply.github.com>
2022-07-03 04:00:30 -0700

    Implemented build card metadata query.

--- CollectionServer/database/queries.js
+++ CollectionServer/database/queries.js
@@ -1,3 +1,5 @@
+const { escape } = require('mysql');
+
 const CREATE_TABLES = `CREATE TABLE IF NOT EXISTS card_infos (
     scryfall_id VARCHAR(36),
     card_name VARCHAR(255),
@@ -12,6 +14,40 @@ const CREATE_TABLES = `CREATE TABLE IF NOT EXISTS card_infos (
     UNIQUE INDEX(scryfall_id)
 ) DEFAULT CHARSET utf8mb4`;
 
+// valid args: card_printed_name, scryfall_image_uri, version_string, reference_usd_cent_price
+function buildCardMetadataQuery(id, card_name, language, scryfall_api_uri, scryfall_card_url, args) {
+    var keys = ['scryfall_id', 'card_name', 'lang', 'scryfall_api_uri', 'scryfall_card_url'];
+    var values = [
+        escape(id),
+        escape(card_name),
+        escape(language),
+        escape(scryfall_api_uri),
+        escape(scryfall_card_url)
+    ];
+    if ('card_printed_name' in args) {
+        keys.push('card_printed_name');
+        values.push(escape(args.card_printed_name));
+    }
+    if ('scryfall_image_uri' in args) {
+        keys.push('scryfall_image_uri');
+        values.push(escape(args.scryfall_image_uri));
+    }
+    if ('version_string' in args) {
+        keys.push('version');
+        values.push(escape(args.version_string));
+    }
+    if ('reference_usd_cent_price' in args) {
+        keys.push('reference_usd_cent_price');
+        values.push(escape(args.reference_usd_cent_price));
+    }
+    var updateArray = values.map((value, index) => (keys[index] + '=' + value));
+    return `INSERT INTO
+    card_infos(${keys.join(', ')})
+    VALUES(${values.join(', ')})
+    ON DUPLICATE KEY UPDATE ${updateArray.join(', ')}`;
+}
+
 module.exports = {
     CREATE_TABLES,
+    buildCardMetadataQuery,
 };
