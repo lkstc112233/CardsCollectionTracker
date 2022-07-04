b971754314231255717f6781c1ba8fe75a8c9799
Photon cat <lkstc112233@users.noreply.github.com>
2022-07-04 02:32:20 -0700

    Added binder sql

--- CollectionServer/database/queries.js
+++ CollectionServer/database/queries.js
@@ -10,7 +10,12 @@ const CREATE_TABLES = `CREATE TABLE IF NOT EXISTS card_infos (
     reference_usd_cent_price INT,
     PRIMARY KEY(scryfall_id),
     UNIQUE INDEX(scryfall_id)
-) DEFAULT CHARSET utf8mb4`;
+) DEFAULT CHARSET utf8mb4;
+CREATE TABLE IF NOT EXISTS binder_infos (
+    id INT AUTO_INCREMENT,
+    binder_name VARCHAR(255),
+    PRIMARY KEY(id)
+) DEFAULT CHARSET utf8mb4;`;
 
 const INSERT_INTO_OR_UPDATE_METADATA_TABLE_QUERY = `INSERT INTO
     card_infos(scryfall_id, 
@@ -34,6 +39,10 @@ const INSERT_INTO_OR_UPDATE_METADATA_TABLE_QUERY = `INSERT INTO
     scryfall_image_uri = VALUES(scryfall_image_uri),
     version = VALUES(version),
     reference_usd_cent_price = VALUES(reference_usd_cent_price)`;
+
+const INSERT_INTO_BINDERS_QUERY = `INSERT INTO
+    binder_infos(binder_name)
+    VALUES(?)`;
 
 function buildInsertOrUpdateMetadataTableQuery(count) {
     return `INSERT INTO
@@ -106,6 +115,7 @@ function formCardMetadataQueryValuesFromCardObject(card) {
 module.exports = {
     CREATE_TABLES,
     INSERT_INTO_OR_UPDATE_METADATA_TABLE_QUERY,
+    INSERT_INTO_BINDERS_QUERY,
     buildInsertOrUpdateMetadataTableQuery,
     formCardMetadataQueryValues,
     formCardMetadataQueryValuesFromCardObject,
