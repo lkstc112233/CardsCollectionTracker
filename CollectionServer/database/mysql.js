7f4103a161990dc9990bb36ca73359f211947747
Photon cat <lkstc112233@users.noreply.github.com>
2022-07-04 00:45:14 -0700

    Refactored insert query.

--- CollectionServer/database/mysql.js
+++ CollectionServer/database/mysql.js
@@ -42,7 +42,8 @@ async function init() {
 async function updateCardMetadata(id, card_name, language, scryfall_api_uri, scryfall_card_url, args) {
     return new Promise((acc, rej) => {
         pool.query(
-            queries.buildCardMetadataQuery(id, card_name, language, scryfall_api_uri, scryfall_card_url, args),
+            queries.selectCardMetadataQuery(args),
+            queries.formCardMetadataQueryValues(id, card_name, language, scryfall_api_uri, scryfall_card_url, args),
             err => {
                 if (err) return rej(err);
 
--- CollectionServer/database/queries.js
+++ CollectionServer/database/queries.js
@@ -1,5 +1,3 @@
-const { escape } = require('mysql');
-
 const CREATE_TABLES = `CREATE TABLE IF NOT EXISTS card_infos (
     scryfall_id VARCHAR(36),
     card_name VARCHAR(255),
@@ -14,40 +12,123 @@ const CREATE_TABLES = `CREATE TABLE IF NOT EXISTS card_infos (
     UNIQUE INDEX(scryfall_id)
 ) DEFAULT CHARSET utf8mb4`;
 
+const INSERT_INTO_OR_UPDATE_METADATA_TABLE_5 = `INSERT INTO
+    card_infos(scryfall_id, card_name, lang, scryfall_api_uri, scryfall_card_url)
+    VALUES(?, ?, ?, ?, ?)
+    ON DUPLICATE KEY UPDATE
+    scryfall_id=VALUES(scryfall_id),
+    card_name=VALUES(card_name),
+    lang=VALUES(lang),
+    scryfall_api_uri=VALUES(scryfall_api_uri),
+    scryfall_card_url=VALUES(scryfall_card_url)`;
+
+const INSERT_INTO_OR_UPDATE_METADATA_TABLE_6 = `INSERT INTO
+    card_infos(scryfall_id, card_name, lang, scryfall_api_uri, scryfall_card_url, ??)
+    VALUES(?, ?, ?, ?, ?, ?)
+    ON DUPLICATE KEY UPDATE
+    scryfall_id=VALUES(scryfall_id),
+    card_name=VALUES(card_name),
+    lang=VALUES(lang),
+    scryfall_api_uri=VALUES(scryfall_api_uri),
+    scryfall_card_url=VALUES(scryfall_card_url),
+    ?? = ??`;
+
+const INSERT_INTO_OR_UPDATE_METADATA_TABLE_7 = `INSERT INTO
+    card_infos(scryfall_id, card_name, lang, scryfall_api_uri, scryfall_card_url, ??, ??)
+    VALUES(?, ?, ?, ?, ?, ?, ?)
+    ON DUPLICATE KEY UPDATE
+    scryfall_id=VALUES(scryfall_id),
+    card_name=VALUES(card_name),
+    lang=VALUES(lang),
+    scryfall_api_uri=VALUES(scryfall_api_uri),
+    scryfall_card_url=VALUES(scryfall_card_url),
+    ?? = ??,
+    ?? = ??`;
+
+const INSERT_INTO_OR_UPDATE_METADATA_TABLE_8 = `INSERT INTO
+    card_infos(scryfall_id, card_name, lang, scryfall_api_uri, scryfall_card_url, ??, ??, ??)
+    VALUES(?, ?, ?, ?, ?, ?, ?, ?)
+    ON DUPLICATE KEY UPDATE
+    scryfall_id=VALUES(scryfall_id),
+    card_name=VALUES(card_name),
+    lang=VALUES(lang),
+    scryfall_api_uri=VALUES(scryfall_api_uri),
+    scryfall_card_url=VALUES(scryfall_card_url),
+    ?? = ??,
+    ?? = ??,
+    ?? = ??`;
+
+const INSERT_INTO_OR_UPDATE_METADATA_TABLE_9 = `INSERT INTO
+    card_infos(scryfall_id, card_name, lang, scryfall_api_uri, scryfall_card_url, ??, ??, ??, ??)
+    VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)
+    ON DUPLICATE KEY UPDATE
+    scryfall_id=VALUES(scryfall_id),
+    card_name=VALUES(card_name),
+    lang=VALUES(lang),
+    scryfall_api_uri=VALUES(scryfall_api_uri),
+    scryfall_card_url=VALUES(scryfall_card_url),
+    ?? = ??,
+    ?? = ??,
+    ?? = ??,
+    ?? = ??`;
+
+const INSERT_INTO_OR_UPDATE_METADATA_TABLE_QUERIES = [
+    INSERT_INTO_OR_UPDATE_METADATA_TABLE_5,
+    INSERT_INTO_OR_UPDATE_METADATA_TABLE_6,
+    INSERT_INTO_OR_UPDATE_METADATA_TABLE_7,
+    INSERT_INTO_OR_UPDATE_METADATA_TABLE_8,
+    INSERT_INTO_OR_UPDATE_METADATA_TABLE_9,
+];
+
 // valid args: card_printed_name, scryfall_image_uri, version_string, reference_usd_cent_price
-function buildCardMetadataQuery(id, card_name, language, scryfall_api_uri, scryfall_card_url, args) {
-    var keys = ['scryfall_id', 'card_name', 'lang', 'scryfall_api_uri', 'scryfall_card_url'];
+function selectCardMetadataQuery(args) {
+    var additionalValueCount = 0;
+    if ('card_printed_name' in args) {
+        ++additionalValueCount;
+    }
+    if ('scryfall_image_uri' in args) {
+        ++additionalValueCount;
+    }
+    if ('version_string' in args) {
+        ++additionalValueCount;
+    }
+    if ('reference_usd_cent_price' in args) {
+        ++additionalValueCount;
+    }
+    return INSERT_INTO_OR_UPDATE_METADATA_TABLE_QUERIES[additionalValueCount];
+}
+
+// valid args: card_printed_name, scryfall_image_uri, version_string, reference_usd_cent_price
+function formCardMetadataQueryValues(id, card_name, language, scryfall_api_uri, scryfall_card_url, args) {
+    var keys = [];
     var values = [
-        escape(id),
-        escape(card_name),
-        escape(language),
-        escape(scryfall_api_uri),
-        escape(scryfall_card_url)
+        id,
+        card_name,
+        language,
+        scryfall_api_uri,
+        scryfall_card_url,
     ];
     if ('card_printed_name' in args) {
         keys.push('card_printed_name');
-        values.push(escape(args.card_printed_name));
+        values.push(args.card_printed_name);
     }
     if ('scryfall_image_uri' in args) {
         keys.push('scryfall_image_uri');
-        values.push(escape(args.scryfall_image_uri));
+        values.push(args.scryfall_image_uri);
     }
     if ('version_string' in args) {
         keys.push('version');
-        values.push(escape(args.version_string));
+        values.push(args.version_string);
     }
     if ('reference_usd_cent_price' in args) {
         keys.push('reference_usd_cent_price');
-        values.push(escape(args.reference_usd_cent_price));
+        values.push(args.reference_usd_cent_price);
     }
-    var updateArray = values.map((value, index) => (keys[index] + '=' + value));
-    return `INSERT INTO
-    card_infos(${keys.join(', ')})
-    VALUES(${values.join(', ')})
-    ON DUPLICATE KEY UPDATE ${updateArray.join(', ')}`;
+    return keys.concat(values, keys.flatMap(k => [k, k]));
 }
 
 module.exports = {
     CREATE_TABLES,
-    buildCardMetadataQuery,
+    selectCardMetadataQuery,
+    formCardMetadataQueryValues,
 };
