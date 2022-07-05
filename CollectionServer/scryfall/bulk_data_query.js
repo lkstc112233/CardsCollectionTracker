6228810e101919188845158b0945a1d24c7f5a27
Photon cat <lkstc112233@users.noreply.github.com>
2022-07-05 01:35:10 -0700

    Added bulk set handling

--- CollectionServer/scryfall/bulk_data_query.js
+++ CollectionServer/scryfall/bulk_data_query.js
@@ -1,5 +1,6 @@
 const got = require('got');
 const { parser } = require('stream-json/Parser');
+const Pick = require('stream-json/filters/Pick');
 const { streamArray } = require('stream-json/streamers/StreamArray')
 const db = require('../database/mysql');
 
@@ -24,6 +25,44 @@ function getAllCardsUrl() {
         });
 }
 
+async function handleAllSets(batch_size = DEFAULT_BATCH_SIZE) {
+    const pipeline = got.stream('https://api.scryfall.com/sets')
+        .pipe(Pick.withParser({filter: 'data'}))
+        .pipe(streamArray());
+    return new Promise(function(resolve, reject) {
+        var objectCounter = 0;
+        var setsList = [];
+        pipeline.on('data', data => {
+            if (data.value?.object === 'set') {
+                ++objectCounter;
+                setsList.push(buildSetObject(data.value));
+                if (setsList.length >= batch_size) {
+                    db.updateSetObjectsMetadata(setsList).catch(err => {
+                        console.log('Error happened while handling all sets: ' + 
+                        err.message + 
+                        ', handling set list ' + 
+                        setsList.map(set => set.name).join(', '));
+                    });
+                    setsList = [];
+                }
+            }
+        });
+        pipeline.on('end', () => {
+            console.log(`Found ${objectCounter} objects.`);
+            if (setsList.length > 0) {
+                db.updateSetObjectsMetadata(setsList).catch(err => {
+                    console.log('Error happened while handling all sets: ' + 
+                    err.message + 
+                    ', handling set list ' + 
+                    setsList.map(set => set.name).join(', '));
+                });
+                setsList = [];
+            }
+            resolve(objectCounter);
+        });
+    });
+}
+
 async function handleAllCards(batch_size = DEFAULT_BATCH_SIZE) {
     var bulk_data_url = await getAllCardsUrl();
     const pipeline = got.stream(bulk_data_url).pipe(parser()).pipe(streamArray());
@@ -33,7 +72,7 @@ async function handleAllCards(batch_size = DEFAULT_BATCH_SIZE) {
         pipeline.on('data', data => {
             if (data.value?.object === 'card') {
                 ++objectCounter;
-                cardsList.push(buildCardsObject(data.value));
+                cardsList.push(buildCardObject(data.value));
                 if (cardsList.length >= batch_size) {
                     updateCards(cardsList).catch(err => {
                         console.log('Error happened while handling all cards: ' + 
@@ -61,7 +100,7 @@ async function handleAllCards(batch_size = DEFAULT_BATCH_SIZE) {
     });
 }
 
-function buildCardsObject(cardData) {
+function buildCardObject(cardData) {
     optionalArgs = {}
     if ('printed_name' in cardData) {
         optionalArgs.card_printed_name = cardData.printed_name;
@@ -85,8 +124,21 @@ function buildCardsObject(cardData) {
     };
 }
 
+function buildSetObject(setData) {
+    return {
+        'id': setData.id,
+        'name': setData.name,
+        'code': setData.code,
+        'uri': setData.uri,
+        'icon_svg_uri': setData.icon_svg_uri,
+    };
+}
+
 async function updateCards(cardList) {
     return db.updateCardObjectsMetadata(cardList);
 }
 
-module.exports = {handleAllCards};
+module.exports = {
+    handleAllCards,
+    handleAllSets,
+};
