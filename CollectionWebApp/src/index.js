9e3dac5b011c07bbe5dacb8f0c4c531c006a3b13
Photon cat <lkstc112233@users.noreply.github.com>
2022-07-08 02:00:26 -0700

    Rename cards

--- CollectionWebApp/src/index.js
+++ CollectionWebApp/src/index.js
@@ -3,7 +3,7 @@ const grpc = require('./grpc');
 const DragSelect = require('dragselect');
 
 grpc.listAllBinderCards().then(listResponse => {
-    document.getElementById('cards').innerHTML = `
+    document.getElementById('cards-collection').innerHTML = `
         ${listResponse.getCardsList()
             .map(card => create_card_dom(card))
             .map(cardDom => '<div class="drag-card">' + cardDom + '</div>')
