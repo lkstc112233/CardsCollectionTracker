76ae479b8a1f3df1f06d8a7dcc41768b344751c3
Photon cat <lkstc112233@users.noreply.github.com>
2022-07-08 01:35:49 -0700

    Integrated with DragSelect.

--- CollectionWebApp/src/index.js
+++ CollectionWebApp/src/index.js
@@ -1,15 +1,15 @@
 const { create_card_dom } = require('./dom/create_card');
 const grpc = require('./grpc');
+const DragSelect = require('dragselect');
 
 grpc.listAllBinderCards().then(listResponse => {
     document.getElementById('cards').innerHTML = `
-        <table id="all-cards">
-        <tr>
         ${listResponse.getCardsList()
             .map(card => create_card_dom(card))
-            .map(cardDom => '<th>' + cardDom + '</th>')
+            .map(cardDom => '<div class="drag-card">' + cardDom + '</div>')
             .join('')}
-        </tr>
-        </table>
         `;
+    new DragSelect({
+        selectables: document.getElementsByClassName('drag-card')
+    });
 });
