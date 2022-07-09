f376e059393fe3fcdcca487727ebeb2772c15eaa
Photon cat <lkstc112233@users.noreply.github.com>
2022-07-09 02:52:41 -0700

    Fix card size

--- CollectionWebApp/src/dom/create_card.js
+++ CollectionWebApp/src/dom/create_card.js
@@ -1,5 +1,5 @@
 function createCardDom(card) {
-    var image = `<img id="card-${card.getId()}-image" src="${card.getCardInfo().getImageUri()}" />`;
+    var image = `<img class="card-img" id="card-${card.getId()}-image" src="${card.getCardInfo().getImageUri()}" />`;
     var text = `<div class="card-name" id="card-${card.getId()}-name">${card.getCardInfo().getName()}</div>`;
     return `<div class="card-box" id="card-${card.getId()}-div">
         <div>${image}</div>
@@ -8,7 +8,7 @@ function createCardDom(card) {
 }
 
 function createCardInfoDom(card) {
-    var image = `<img id="card-${card.getId()}-image" src="${card.getImageUri()}" />`;
+    var image = `<img class="card-img" id="card-${card.getId()}-image" src="${card.getImageUri()}" />`;
     var text = `<div class="card-name" id="card-${card.getId()}-name">${card.getName()}</div>`;
     return `<div class="card-box" id="card-${card.getId()}-div">
         <div>${image}</div>
