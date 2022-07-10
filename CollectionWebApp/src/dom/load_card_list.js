d06d1ad75524319a7fb30928886ec8f6863a498d
Photon cat <lkstc112233@users.noreply.github.com>
2022-07-10 04:13:34 -0700

    Use async-await idiom

--- CollectionWebApp/src/dom/load_card_list.js
+++ CollectionWebApp/src/dom/load_card_list.js
@@ -3,14 +3,13 @@ const grpc = require('../grpc');
 const DragSelect = require('dragselect');
 
 async function loadBinderDom(binder = 0) {
-    return grpc.listAllBinderCards(binder).then(listResponse => {
-        document.getElementById('cards-collection').innerHTML = listResponse.getCardsList()
-            .map(card => createCardDom(card))
-            .map(cardDom => '<div class="drag-card">' + cardDom + '</div>')
-            .join('');
-        new DragSelect({
-            selectables: document.getElementsByClassName('drag-card')
-        });
+    listResponse = await grpc.listAllBinderCards(binder);
+    document.getElementById('cards-collection').innerHTML = listResponse.getCardsList()
+        .map(card => createCardDom(card))
+        .map(cardDom => '<div class="drag-card">' + cardDom + '</div>')
+        .join('');
+    new DragSelect({
+        selectables: document.getElementsByClassName('drag-card')
     });
 }
 
@@ -19,10 +18,9 @@ async function loadSearchAddListDom(query = '') {
     if (query === '') {
         return;
     }
-    return grpc.queryCardInfoByName(query).then(listResponse => {
-        document.getElementById('search-add-list')
-            .replaceChildren(...listResponse.getInfoList().map(card => createCardInfoDom(card)));
-    });
+    listResponse = await grpc.queryCardInfoByName(query);
+    document.getElementById('search-add-list')
+        .replaceChildren(...listResponse.getInfoList().map(card => createCardInfoDom(card)));
 }
 
 module.exports = {
