1807562d8a8e1c9dee88d2a7419ad93fc20013df
Photon cat <lkstc112233@users.noreply.github.com>
2022-07-10 15:38:01 -0700

    Updated some responses for fake server.
    Also added a delay feature to test features that expected to be slow.

--- FakeCollectionServer/server.js
+++ FakeCollectionServer/server.js
@@ -1,10 +1,15 @@
 const grpc = require('./grpc');
 
-function bindFakeHandler(rpc_name, response) {
+function bindFakeHandler(rpc_name, response, delayMs) {
     grpc.bindRpcHandler(rpc_name, function (call, callback) {
         console.log(`#${rpc_name} Request: ` + JSON.stringify(call.request, null, 4));
         console.log(`#${rpc_name} Response: ` + JSON.stringify(response, null, 4));
-        callback(null, response);
+        if (delayMs !== undefined && delayMs > 0) {
+            new Promise(resolve => setTimeout(() => resolve(), delayMs))
+            .then(() => callback(null, response));
+        } else {
+            callback(null, response);
+        }
     });
 }
 
@@ -12,9 +17,9 @@ bindFakeHandler('updateMetadata', {
     sets_downloaded: 0,
     cards_downloaded: 0,
     oracle_downloaded: 0
-});
+}, 2000);
 bindFakeHandler('addBinder', {});
-bindFakeHandler('listBinders', { binders: [] });
+bindFakeHandler('listBinders', { binders: [{name: 'Unbinded', id: 1}, {name: 'binder 1', id: 10}, ] });
 bindFakeHandler('updateBinder', {});
 bindFakeHandler('deleteBinder', {});
 bindFakeHandler('queryCardInfoByName', {
@@ -24,7 +29,13 @@ bindFakeHandler('queryCardInfoByName', {
         image_uri: 'https://via.placeholder.com/300.png',
         printed_name: 'print test',
         language: 'en',
-        versions: 'nonfoil|foil',
+        versions: ['nonfoil', 'foil'],
+    }, {
+        id: '00000000-0000-0000-0000-000000000000',
+        name: 'test 2',
+        image_uri: 'https://via.placeholder.com/300.png',
+        printed_name: 'print test 2',
+        language: 'en',
     }]
 });
 bindFakeHandler('addCardToCollection', {});
