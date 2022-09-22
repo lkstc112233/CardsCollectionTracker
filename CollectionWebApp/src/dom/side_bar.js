51429be2248b43d3caa55712a6c67ef2dbdcc7c3
Photon cat <lkstc112233@users.noreply.github.com>
2022-09-21 23:57:29 -0700

    Extend sidebar width.

--- CollectionWebApp/src/dom/side_bar.js
+++ CollectionWebApp/src/dom/side_bar.js
@@ -12,9 +12,9 @@ function toggleSidebar() {
         document.getElementById("binder-sidebar").style.left = "calc(100% - 50px)";
         document.getElementById("main-panel").style.marginRight = "50px";
     } else {
-        document.getElementById("binder-sidebar").style.width = "250px";
-        document.getElementById("binder-sidebar").style.left = "calc(100% - 250px)";
-        document.getElementById("main-panel").style.marginRight = "250px";
+        document.getElementById("binder-sidebar").style.width = "400px";
+        document.getElementById("binder-sidebar").style.left = "calc(100% - 400px)";
+        document.getElementById("main-panel").style.marginRight = "400px";
     }
     sidebarOpened = !sidebarOpened;
 }
