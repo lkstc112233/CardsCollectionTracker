9c4ab27f5f06c942c753d3ff4ed1016ccebe5421
Photon cat <lkstc112233@users.noreply.github.com>
2022-09-25 19:59:22 -0700

    Added a popup to show deck in content.

---
+++ CollectionExtension/popup.js
@@ -0,0 +1,29 @@
+async function fetchData() {
+    return new Promise(acc => {
+        chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+            chrome.tabs.sendMessage(tabs[0].id, {command: "fetchDecks"}, (response) => {
+                acc(response);
+            });
+        });
+    });
+}
+
+async function updatePlugin() {
+    data = await fetchData();
+    document.getElementById('decks').replaceChildren(...data.map(
+        deck => {
+            var titleDom = document.createElement('h3');
+            titleDom.innerText = deck.name;
+            var deckDom = document.createElement('div');
+            deckDom.innerText = deck.cards.map(
+                c => c.count + ' ' + c.name
+            ).join('\n');
+            var holderDom = document.createElement('div');
+            holderDom.appendChild(titleDom);
+            holderDom.appendChild(deckDom);
+            return holderDom;
+        }
+    ));
+}
+
+document.getElementById('fetch').addEventListener('click', updatePlugin);
