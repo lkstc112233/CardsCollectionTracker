18bce7c1890744776e0ab87786652132d0dec767
Photon cat <lkstc112233@users.noreply.github.com>
2022-09-26 20:48:11 -0700

    Created skeleton mobile application

---
+++ CollectionMobileApp/CollectionMobileApp/CollectionMobileAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CollectionMobileAppApp.swift
+//  CollectionMobileApp
+//
+//  Created by Kevin on 9/26/22.
+//
+
+import SwiftUI
+
+@main
+struct CollectionMobileAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
