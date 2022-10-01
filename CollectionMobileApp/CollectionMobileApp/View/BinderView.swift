be18c857ef7604b530453b0eafb5ec644e13d5fb
Photon cat <lkstc112233@users.noreply.github.com>
2022-10-01 14:05:01 -0700

    Added the button to navigate to add card page

--- CollectionMobileApp/CollectionMobileApp/View/BinderView.swift
+++ CollectionMobileApp/CollectionMobileApp/View/BinderView.swift
@@ -12,47 +12,56 @@ struct BinderView: View {
     var id: Int32
     @State var cards: [IdentifiableWrapper<CardCollection_Card, Int32>] = []
     @State var error: Bool = false
+    @State var addingCard: Bool = false
     
     var body: some View {
-        if error {
-            VStack {
-                Text("Unable to load collections")
-                Button("Retry") {
-                    Task {
-                        await loadCardsInBinder()
+        NavigationView {
+            if error {
+                VStack {
+                    Text("Unable to load collections")
+                    Button("Retry") {
+                        Task {
+                            await loadCardsInBinder()
+                        }
                     }
                 }
-            }
-            .navigationTitle(name)
-        } else {
-            List(cards) { card in
-                HStack {
-                    if card.value.cardInfo.printedName != "" {
-                        Text(card.value.cardInfo.printedName)
-                    } else {
-                        Text(card.value.cardInfo.name)
-                    }
-                    if card.value.version != "" {
-                        Text(" (\(card.value.version))")
+                .navigationTitle(name)
+                .navigationBarTitleDisplayMode(.inline)
+            } else {
+                List(cards) { card in
+                    HStack {
+                        if card.value.cardInfo.printedName != "" {
+                            Text(card.value.cardInfo.printedName)
+                        } else {
+                            Text(card.value.cardInfo.name)
+                        }
+                        if card.value.version != "" {
+                            Text(" (\(card.value.version))")
+                                .foregroundColor(.secondary)
+                        }
+                        Spacer()
+                        Text(card.value.cardInfo.setName)
                             .foregroundColor(.secondary)
                     }
-                    Spacer()
-                    Text(card.value.cardInfo.setName)
-                        .foregroundColor(.secondary)
+                }
+                .refreshable {
+                    await loadCardsInBinder()
+                }
+                .navigationTitle(name)
+                .toolbar{
+                    Button{
+                        addingCard.toggle()
+                    } label: {
+                        Label("Add card", systemImage: "plus.circle")
+                    }
+                }
+                .sheet(isPresented: $addingCard) {
+                    AddCardView(id:id)
                 }
             }
-            .refreshable {
-                await loadCardsInBinder()
-            }
-            .navigationTitle(name)
-            .toolbar{
-                Button{} label: {
-                    Label("Cache", systemImage: "archivebox.fill")
-                }
-            }
-            .task{
-                await loadCardsInBinder()
-            }
+        }
+        .task{
+            await loadCardsInBinder()
         }
     }
     
