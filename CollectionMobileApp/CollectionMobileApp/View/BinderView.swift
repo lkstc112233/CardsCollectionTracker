53544a44b902afd33958475102d26d3628e7b67b
Photon cat <lkstc112233@users.noreply.github.com>
2022-10-01 02:38:39 -0700

    Added card list call.

--- CollectionMobileApp/CollectionMobileApp/View/BinderView.swift
+++ CollectionMobileApp/CollectionMobileApp/View/BinderView.swift
@@ -10,46 +10,62 @@ import SwiftUI
 struct BinderView: View {
     var name: String
     var id: Int32
+    @State var cards: [IdentifiableWrapper<CardCollection_Card, Int32>] = []
+    @State var error: Bool = false
     
     var body: some View {
-        List {
-            Text("Example Card 1")
-                .contextMenu {
-                    Button {
-                    }label: {
-                        Label("Delete from collection", systemImage: "minus.circle")
+        if error {
+            VStack {
+                Text("Unable to load collections")
+                Button("Retry") {
+                    Task {
+                        await loadCardsInBinder()
                     }
                 }
-            Text("Example Card 2")
-                .contextMenu {
-                    Button {
-                    }label: {
-                        Label("Delete from collection", systemImage: "minus.circle")
+            }
+            .navigationTitle(name)
+        } else {
+            List(cards) { card in
+                HStack {
+                    if card.value.cardInfo.printedName != "" {
+                        Text(card.value.cardInfo.printedName)
+                    } else {
+                        Text(card.value.cardInfo.name)
                     }
-                }
-            Text("Example Card 3")
-                .contextMenu {
-                    Button {
-                    }label: {
-                        Label("Delete from collection", systemImage: "minus.circle")
+                    if card.value.version != "" {
+                        Text(" (\(card.value.version))")
+                            .foregroundColor(.secondary)
                     }
+                    Spacer()
+                    Text(card.value.cardInfo.setName)
+                        .foregroundColor(.secondary)
                 }
-            Text("Example Card 4")
-                .contextMenu {
-                    Button {
-                    }label: {
-                        Label("Delete from collection", systemImage: "minus.circle")
-                    }
+            }
+            .refreshable {
+                await loadCardsInBinder()
+            }
+            .navigationTitle(name)
+            .toolbar{
+                Button{} label: {
+                    Label("Cache", systemImage: "archivebox.fill")
                 }
-        }
-        .navigationTitle(name)
-        .toolbar{
-            Button{} label: {
-                Label("Cache", systemImage: "archivebox.fill")
+            }
+            .task{
+                await loadCardsInBinder()
             }
         }
-        .refreshable {
-            // gRPC
+    }
+    
+    func loadCardsInBinder() async {
+        do {
+            self.cards = try await GrpcClient.listCardsInBinder(id: id ).map({ card in
+                wrapIdentifiable(value: card, getId: {c in c.id})
+            })
+            error = false
+        } catch {
+            self.error = true
+            self.cards = []
+            print("Error happened loading binders: " + error.localizedDescription)
         }
     }
 }
