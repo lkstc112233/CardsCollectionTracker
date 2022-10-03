87a7e7758240b99a786fb98d893ed02a6143b3d1
Photon cat <lkstc112233@users.noreply.github.com>
2022-10-03 00:51:30 -0700

    Added a query method for scryfall client.

--- CollectionMobileApp/CollectionMobileApp/Utils/ScryfallClient.swift
+++ CollectionMobileApp/CollectionMobileApp/Utils/ScryfallClient.swift
@@ -7,11 +7,29 @@
 
 import Foundation
 
+let ScryfallClient = ScryfallClientImpl()
+
 enum ScryfallError: Error {
     case invalidUrl
+    case responseError
 }
 
-class ScryfallClient {
+struct ScryfallCard {
+    var name: String?
+    var scryfallId: String?
+    var imageUrl: String?
+    var setName: String?
+    var collectorId: String?
+    var versions: [String]
+}
+
+fileprivate struct DecodeScryfallCardInfo {
+    
+}
+
+class ScryfallClientImpl {
+    
+    fileprivate init() {}
     
     private static func generateScryfallCardQueryParams(name: String, allLang: Bool) -> [URLQueryItem] {
         var params : [URLQueryItem] = []
@@ -37,6 +55,39 @@ class ScryfallClient {
         }
         
         return url
+    }
+    
+    func queryCardFromScryfall(name: String, allLang: Bool = false) throws -> [ScryfallCard] {
+        var dataReceived: Data?
+        var error: Error?
+        let sem = DispatchSemaphore.init(value: 0)
+        let url = try ScryfallClientImpl.generateScryfallCardQueryUrl(name: name, allLang: allLang)
+        URLSession.shared.dataTask(with: url) {
+            data, request, errorInRequest in
+            defer {
+                sem.signal()
+            }
+            if let e = errorInRequest {
+                print("Error -> \(e)")
+                error = e
+                return
+            }
+            dataReceived = data
+        }.resume()
+        sem.wait()
+        
+        if let e = error {
+            throw e
+        }
+        
+        guard let data = dataReceived else {
+            throw ScryfallError.responseError
+        }
+        
+        print("Data received:")
+        print(String(decoding: data, as: UTF8.self))
+        
+        return []
     }
 }
 
